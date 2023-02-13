28f8f7a5e44d0c05be017e6a53fdd3a88ee2d6a4
insanaja <125232802+insanaja@users.noreply.github.com>
2023-02-13 09:55:19 +0700

    Add files via upload

---
+++ insan kamil_x tkj2/latihan_2.py
@@ -0,0 +1,10 @@
+def segitiga(alas, tinggi):
+    luas =1/2 * alas * tinggi/2
+    print("Luas segitiga adalah:%i cm"%luas)
+segitiga(25,30)
+
+def persegi_panjang(panjang,lebar):
+    luas = panjang*lebar
+    print("luas persegi panjang adalah:%i"%luas)
+
+persegi_panjang(20,30)
---
+++ insan kamil_x tkj2/latihan_fungsi.py
@@ -0,0 +1,47 @@
+'''Fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("Assalamu'alaikum !! Akhi Ukhty")
+
+salam()
+
+'''Buatlah sebuah fungsi versi kalian'''
+ 
+def barudak():
+    print("well buakarr")
+
+barudak()
+barudak()
+barudak()
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("Halo perkenalakan nama saya", nama)
+
+say_hi("Vensen")
+say_hi("fardu")
+
+'''Buatlah fungsi dengar tiga parameter'''
+
+def orang_keren(orang1, orang2, orang3):
+    print("orang paling keren adalah"+" "+ orang1)
+    print("Orang paling keren adalah"+" "+ orang2)
+    print("Orang paling keren adalah"+" "+ orang3)
+
+orang_keren(orang1 = "Kamil", orang2 = "Rasya", orang3 = "Bayu")
+
+def perkenalan(Nama, alamat, umur):
+    print("Nama saya:", Nama)
+    print("alamat saya:", alamat)
+    print("Umur saya:", umur, 'tahun')
+
+perkenalan("Bagas,", "Bandung", 20)
+
+print("\n\n===========BATAS===========")
+
+perkenalan("Azril", "Bogor", 18)
+
